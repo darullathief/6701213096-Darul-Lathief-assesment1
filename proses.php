905965b5563eb8949fa0d31f18b08c8af1c41850
darullathief <darullathief@gmail.com>
2023-03-27 09:24:34 +0700

    v1.2.1

---
+++ proses.php
@@ -0,0 +1,30 @@
+<?php 
+$operator = $_GET['operator'];
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+$total = 0;
+
+switch ($operator) {
+    case '+':
+        $total = $num1 + $num2;
+        echo"Penjumlahan : ";
+        echo"$num1 + $num2 = $total";
+        break;
+    case '-':
+        $total = $num1 - $num2;
+        echo"Pengurangan : ";
+        echo"$num1 - $num2 = $total";
+        break;
+    case '*':
+        $total = $num1 * $num2;
+        echo"Perkalian : ";
+        echo"$num1 x $num2 = $total";
+        break;
+    case '/':
+        $total = $num1 / $num2;
+        echo"Pembagian : ";
+        echo"$num1 / $num2 = $total";
+        break;
+    
+}
+?>
